aa94c910686a3d633355e7675fb5a5afc90dd667
radhika Kakkar <radhikakakkar@radhikas-MacBook-Air.local>
2022-05-09 11:32:03 +0530

    initial commit

---
+++ src/components/create-album/CreateAlbum.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function CreateAlbum() {
+  return (
+    <div className='create-album-container'>
+      <div className='create-album-greeting'>
+        <span>HELLO USER</span>
+      </div>
+      <div className='create-album-form-container'>
+        <div className='create-album-form'>
+          <form>
+          
+          </form>
+        </div>
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default CreateAlbum
